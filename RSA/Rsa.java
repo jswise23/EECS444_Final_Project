1b2cf374b17fa2fedbffa1a0f151e216aadd5b26
Jacob Wise <jswise23@gmail.com>
2018-11-30 13:46:33 -0500

    Added Brian's updates

--- RSA/Rsa.java
+++ RSA/Rsa.java
@@ -1,17 +1,45 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 public class Rsa {
+    private static final int DEFAULT_KEY_SIZE = 2048;
+    private static final int DEFAULT_PUBLIC_EXP = 65537;
+    private static final BigInteger ONE = BigInteger.ONE;
+    private static final BigInteger ZERO = BigInteger.ZERO;
 
     /**
      * This method generates a new KeyPair object
      * @return This returns a KeyPair object used for RSA
      */
-    private static KeyPair generateKeyPair() {
-        // TODO: Implement algorithm for creating each of these BigInteger objects
-        BigInteger publicKey = null;
-        BigInteger publicKeyExp = null;
-        BigInteger privateKey = null;
-        return new KeyPair(publicKey, publicKeyExp, privateKey);
+    private static KeyPair generateKeyPair(final int keySize) {
+        while(true) {
+            final BigInteger p = getRandomPrime(keySize / 2);
+            final BigInteger e = BigInteger.valueOf(DEFAULT_PUBLIC_EXP);
+            final BigInteger q = getRandomPrime(keySize / 2);
+            final BigInteger lambda = lcm(p.subtract(ONE), q.subtract(ONE));
+
+            if(validKeyPair(keySize, lambda, p, e, q)) {
+                return new KeyPair(p.multiply(q), e, e.modInverse(lambda));
+            }
+        }
+    }
+
+    private static BigInteger getRandomPrime(final int bitLength) {
+        return BigInteger.probablePrime(bitLength, new Random());
+    }
+
+    private static boolean validKeyPair(final int keySize,
+            final BigInteger lambda,
+            final BigInteger p,
+            final BigInteger e,
+            final BigInteger q) {
+
+        return e.gcd(lambda).equals(ONE) && 
+                p.subtract(q).abs().subtract(ONE.shiftLeft(keySize / 2 - 100)).signum() == 1;
+    }
+
+    private static BigInteger lcm(BigInteger a, BigInteger b) {
+        return a.multiply(b.divide(a.gcd(b)));
     }
 
     /**
@@ -21,12 +49,12 @@ public class Rsa {
      * @param e This is the public key exponent
      * @return byte[] This returns the encrypted message
      */
-    private static byte[] encrypt(byte[] m, byte[] n, byte[] e) {
-        final BigInteger message = new BigInteger(m);
-        final BigInteger publicKey = new BigInteger(n);
-        final BigInteger publicKeyExp = new BigInteger(e);
-        final BigInteger encryptedMessage = message.modPow(publicKeyExp, publicKey);
-        return encryptedMessage.toByteArray();
+    private static byte[] encrypt(final byte[] m, final byte[] n, final byte[] e) {
+        return encrypt(new BigInteger(m), new BigInteger(n), new BigInteger(e)).toByteArray();
+    }
+
+    public static BigInteger encrypt(final BigInteger m, final BigInteger n, final BigInteger e) {
+        return m.modPow(e, n);
     }
 
     /**
@@ -36,18 +64,20 @@ public class Rsa {
      * @param n This is the public key
      * @return byte[] This returns the decrypted message
      */
-    private static byte[] decrypt(byte[] c, byte[] d, byte[] n) {
-        final BigInteger encryptedMessage = new BigInteger(c);
-        final BigInteger privateKey = new BigInteger(d);
-        final BigInteger publicKey = new BigInteger(n);
-        final BigInteger decryptedMessage = encryptedMessage.modPow(privateKey, publicKey);
-        return decryptedMessage.toByteArray();
+    private static byte[] decrypt(final byte[] c, final byte[] d, final byte[] n) {
+        return decrypt(new BigInteger(c), new BigInteger(d), new BigInteger(n)).toByteArray();
+    }
+
+    private static BigInteger decrypt(final BigInteger c, final BigInteger d, final BigInteger n) {
+        return c.modPow(d, n);
     }
 
     private static class KeyPair {
         private final byte[] publicKey, publicKeyExp, privateKey;
 
-        KeyPair(BigInteger publicKey, BigInteger publicKeyExp, BigInteger privateKey) {
+        KeyPair(final BigInteger publicKey, 
+                final BigInteger publicKeyExp,
+                final BigInteger privateKey) {
             this.publicKey = publicKey.toByteArray();
             this.publicKeyExp = publicKeyExp.toByteArray();
             this.privateKey = privateKey.toByteArray();
@@ -64,11 +94,37 @@ public class Rsa {
         public byte[] getPrivateKey() {
             return privateKey;
         }
+
+        @Override
+        public String toString() {
+            return String.format("Public: %s, Exp: %s, Private: %s", new BigInteger(publicKey).toString(), new BigInteger(publicKeyExp).toString(), new BigInteger(privateKey).toString());
+        }
+    }
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
     public static void main(String[] args) {
-        KeyPair keyPair = generateKeyPair();
-        // Can use this for testing
+        final KeyPair keyPair = generateKeyPair(DEFAULT_KEY_SIZE);
+        System.out.println(keyPair);
+        final byte[] publicKey = keyPair.getPublicKey();
+        final byte[] publicKeyExp = keyPair.getPublicKeyExp();
+        final byte[] privateKey = keyPair.getPrivateKey();
 
+        final String message = "Hello world";
+        final byte[] messageBytes = message.getBytes();
+        System.out.printf("Original bytes: %s\n", bytesToHex(messageBytes));
+        final byte[] encryptedBytes = encrypt(messageBytes, publicKey, publicKeyExp);
+        System.out.printf("Encrypted bytes: %s\n", bytesToHex(encryptedBytes));
+        final byte[] decryptedBytes = decrypt(encryptedBytes, privateKey, publicKey);
+        System.out.printf("Decrypted bytes: %s\n", bytesToHex(decryptedBytes));
     }
 }
